14f7f802e46345665c95859c9c019633a716babc
nguyenbt <nguyenbt@git.tandan.com>
2022-05-25 16:38:59 +0700

    sss

--- src/app/pages/home/body/DownloadApp.jsx
+++ src/app/pages/home/body/DownloadApp.jsx
@@ -4,7 +4,17 @@ const DownloadApp = () => {
   return (
     <div className='download__app__container'>
         <div className="row">
-            
+            <div className="col-7">
+
+                <div className="download__app__title">
+                    <h1>Mọi tiện ích trong tầm tay</h1>
+                </div>
+                <div className='line__red' ></div>
+
+
+
+            </div>
+
         </div>
 
       
